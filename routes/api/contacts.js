c0c04070d4119fcbfe945e5bb230305d74768c16
Nikita <evildeus666@gmail.com>
2023-08-06 00:46:40 +0300

    init

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "dasdfasfasfe" });
+  res.json({ message: "dasdfadssfasfe" });
 });
 
 router.get("/:contactId", async (req, res, next) => {
